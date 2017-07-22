25d4cfd05b7312dc0a5a39249ee4e8386ed322a0
JoJo <jojo141a@hotmail.com>
2017-07-22 15:09:56 +0700

    add login class

---
+++ app/src/main/java/com/nongmah/kotlin101/LoginJava.java
@@ -0,0 +1,31 @@
+package com.nongmah.kotlin101;
+
+/**
+ * Created by JoJo on 7/22/17.
+ */
+
+public class LoginJava {
+    private String username;
+    private String password;
+
+    public LoginJava(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/nongmah/kotlin101/LoginKotlin.kt
@@ -0,0 +1,6 @@
+package com.nongmah.kotlin101
+
+/**
+ * Created by JoJo on 7/22/17.
+ */
+class LoginKotlin constructor(val username: String, var password: String)
